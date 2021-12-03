9a87d9ed4744c0a53559279e5a79f45c45ddf26b
Nikita Popov <nikita.popov@itechart-group.com>
2021-12-03 13:22:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import Board from "./components/Board";
+import StatusMessage from "./components/StatusMessage";
+import History from "./components/History";
 import { calculateWinner } from "./helpers";
 
 import "./styles/root.scss"
 
+const NEW_GAME = [
+  { board: Array(9).fill(null), isXNext: true }
+];
+
 const App = () => {
-  const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isXNext: true }
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
 
-  const winner = calculateWinner(current.board);
-
-  console.log(winner);
-
-  const message = winner 
-    ? `Winner is ${winner}` 
-    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+  const { winner, winningSquares } = calculateWinner(current.board);
 
   const handleSquareClick = (position) => {
     if (current.board[position] || winner) {
@@ -41,14 +39,40 @@ const App = () => {
     setCurrentMove(prev => prev + 1);
   }
 
+  const moveTo = (move) => {
+    setCurrentMove(move);
+  }
+
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  }
+
   return (
     <div className="app">
-      <h1>TIC TAC TOE</h1>
-      <h2>{ message }</h2>
+      <h1>TIC <span className="text-green">TAC</span> TOE</h1>
+      <StatusMessage
+        winner={winner}
+        current={current} />
       <Board
         board={current.board}
         handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
       />
+      <button 
+        type="button"
+        onClick={onNewGame}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
+        Start New Game
+      </button>
+      <h3 style={{ fontWeight: 'normal' }}>Current game history</h3>
+      <History
+        history={history}
+        moveTo={moveTo}
+        currentMove={currentMove} 
+      />
+      <div className="bg-balls" />
     </div>
   );
 };
